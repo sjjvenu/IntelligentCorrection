30611dd8c4870be49962f08caf1e91d6734bdc1e
tdx-qinken <qinken547@gmail.com>
2019-05-27 09:51:30 +0800

    init

---
+++ IntelligentCorrection/MainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainViewController.swift
+//  IntelligentCorrection
+//
+//  Created by tdx on 2019/5/27.
+//  Copyright © 2019 sjjvenu. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.red;
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Podfile
@@ -0,0 +1,15 @@
+platform :ios,9.0
+inhibit_all_warnings!
+use_frameworks!
+
+def pods
+    pod 'SnapKit'
+    pod 'MJRefresh'
+    pod 'SDWebImage'
+    pod 'Alamofire'
+    pod 'Toast-Swift'
+end
+
+target 'IntelligentCorrection' do
+    pods
+end
